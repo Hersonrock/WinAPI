c39ff9d8c199297a78863b6152b2d8df76fca5ab
Hersonrock <hjpg20@gmail.com>
2025-08-07 02:03:54 -0500

    Completing WndProc function

--- main.cpp
+++ main.cpp
@@ -2,11 +2,37 @@
 #include <string>
 
 
-LRESULT CALLBACK WndProc(HWND  hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+LRESULT CALLBACK WndProc(HWND  hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE PrevInstance, LPSTR LpCmdLine, int nCmdShow) {
 
-	MessageBox(NULL, "Goodbye, cruel world!", "Note", MB_OK);
+	
 	return 0;
 }
 
+LRESULT CALLBACK WndProc(HWND  hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+
+	case WM_LBUTTONDOWN:
+	{
+		char FileNameC[MAX_PATH];
+		HINSTANCE hInstance = GetModuleHandle(NULL);
+		GetModuleFileName(hInstance, FileNameC, MAX_PATH);
+		MessageBox(hwnd, FileNameC, "This program is:", MB_OK | MB_ICONINFORMATION);
+		break;
+	}
+	case WM_CLOSE:
+	{
+		DestroyWindow(hwnd);
+		break;
+	}
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		break;
+	}
+	default:
+		return  DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
