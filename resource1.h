48e1a900dc91bb401dedbe60ac73f1f72ee8045b
Hersonrock <hjpg20@gmail.com>
2025-08-27 09:44:24 -0500

    Modeless Dialog creation

--- resource1.h
+++ resource1.h
@@ -2,16 +2,22 @@
 // Microsoft Visual C++ generated include file.
 // Used by WinAPI_Project.rc
 //
-#define IDI_ICON1                       101
-#define IDI_ICON2                       102
-#define IDI_ICON3                       103
-#define IDR_MENU1                       104
+#define IDR_MYMENU                      101
+#define IDI_ICON1                       102
+#define IDI_ICON2                       103
+#define IDD_ABOUT                       104
+#define IDD_DIALOG1                     105
+#define IDD_MLDIALOG1                   105
+#define ID_FILE_EXIT                    40001
+#define ID_STUFF_GO                     40002
+#define ID_STUFF_GOSOMEWHEREELSE        40003
+#define ID_HELP_ABOUT                   40004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
